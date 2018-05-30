74b567cadb293e9e31d92a7426e665800e0e8300
Oscar Gibrán Córdoba Méndez <o.cordobam@hotmail.com>
2018-05-29 23:34:34 -0500

    add - winston logger changes

--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,4 @@
 const winston = require('winston');
-const dailyWinston = require('winston-daily-rotate-file');
 const fs = require('fs');
 const env = process.env.NODE_ENV || 'dev';
 const logDir = 'log';
@@ -17,13 +16,6 @@ const logger = new (winston.Logger)({
             timestamp: tsFormat,
             colorize: true,
             level: (env === 'dev') ? 'debug' : 'info'
-        }),
-        new (dailyWinston)({
-            filename: `${logDir}-log`,
-            timestamp: tsFormat,
-            datePattern: 'yyyy-MM-dd',
-            prepend: true,
-            level: (env === 'dev') ? 'verbose' : 'info'
         })
     ]
 });
